513d94696d95938e3d77741d7e6c1a5bffacefff
Justin Chang <justin.p.chang@gmail.com>
2023-01-30 18:03:15 -0500

    Implement all instruction types

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -17,32 +17,23 @@ enum Instruction {
   RotateCCW,
 }
 
-const INSTRUCTIONS: Instruction[] = [Instruction.Forward, Instruction.RotateCW];
-
-interface PlayerState {
-  row: number;
-  col: number;
-  angle: number;
-}
+const INSTRUCTIONS: Instruction[] = [
+  Instruction.Forward,
+  Instruction.RotateCW,
+  Instruction.Forward,
+  Instruction.RotateCCW,
+  Instruction.Forward,
+];
 
 const UNIT_LENGTH = 40;
 const X_OFFSET = 100;
 const Y_OFFSET = 100;
-const TOLERANCE = 4;
+const DURATION = 1000;
+const EASE = "Sine.easeInOut";
 
 export default class GameScene extends Phaser.Scene {
   private walls!: Phaser.Physics.Arcade.StaticGroup;
   private player!: Phaser.GameObjects.Triangle;
-  private state = {
-    instruction: {
-      currentIndex: 0,
-    },
-    player: {
-      row: 0,
-      col: 0,
-      angle: 0,
-    },
-  };
 
   constructor() {
     super("game");
@@ -62,18 +53,41 @@ export default class GameScene extends Phaser.Scene {
     return this.drawTile(row, col).setStrokeStyle(1, 0xffffff).setAlpha(0.1);
   }
 
-  isCurrentInstructionFinished(): boolean {
-    switch (INSTRUCTIONS[this.state.instruction.currentIndex]) {
+  addTween(index: number, target: object): void {
+    this.tweens.add({
+      targets: this.player,
+      duration: DURATION,
+      ease: EASE,
+      onComplete: () => this.startInstruction(index + 1),
+      ...target,
+    });
+  }
+
+  startInstruction(index: number): void {
+    if (index >= INSTRUCTIONS.length) {
+      return;
+    }
+    switch (INSTRUCTIONS[index]) {
       case Instruction.Forward:
-        const movement = new Phaser.Math.Vector2(0, UNIT_LENGTH).setAngle(this.state.player.angle);
-        return (
-          this.player.x === this.state.player.col * UNIT_LENGTH + movement.x &&
-          this.player.y === this.state.player.row * UNIT_LENGTH + movement.y
+        const delta = new Phaser.Math.Vector2(UNIT_LENGTH, 0).setAngle(
+          Phaser.Math.DegToRad(this.player.angle)
         );
+        const target = (<Phaser.Math.Vector2>this.player.body.position).add(delta);
+        this.addTween(index, {
+          x: target.x + UNIT_LENGTH / 2,
+          y: target.y + UNIT_LENGTH / 2,
+        });
+        break;
       case Instruction.RotateCW:
-        return this.player.angle === Phaser.Math.Angle.WrapDegrees(this.state.player.angle + 90);
+        this.addTween(index, {
+          angle: this.player.angle + 90,
+        });
+        break;
       case Instruction.RotateCCW:
-        return this.player.angle === Phaser.Math.Angle.WrapDegrees(this.state.player.angle - 90);
+        this.addTween(index, {
+          angle: this.player.angle - 90,
+        });
+        break;
       default:
         throw `Invalid instruction`;
     }
@@ -85,9 +99,9 @@ export default class GameScene extends Phaser.Scene {
     for (let row = 0; row < MAP.length; row += 1) {
       for (let col = 0; col < MAP[row].length; col += 1) {
         const tile = MAP[row][col];
+        this.drawFloor(row, col);
         switch (tile) {
           case 0: // floors
-            this.drawFloor(row, col);
             break;
           case 1: // walls
             const wall = this.drawTile(row, col);
@@ -95,22 +109,19 @@ export default class GameScene extends Phaser.Scene {
             this.walls.add(wall);
             break;
           case 2: // player
-            this.drawFloor(row, col);
             this.player = this.add.triangle(
               X_OFFSET + col * UNIT_LENGTH,
               Y_OFFSET + row * UNIT_LENGTH,
               0,
-              UNIT_LENGTH,
+              0,
+              0,
               UNIT_LENGTH,
               UNIT_LENGTH,
               UNIT_LENGTH / 2,
-              0,
               0x6666ff
             );
+            this.player.angle = -90;
             this.physics.add.existing(this.player);
-            this.state.player.row = row;
-            this.state.player.col = col;
-            this.state.player.angle = this.player.angle;
             break;
         }
       }
@@ -118,40 +129,10 @@ export default class GameScene extends Phaser.Scene {
 
     // Add colliders
     this.physics.add.collider(this.player, this.walls);
+
+    // Start
+    this.startInstruction(0);
   }
 
-  update(): void {
-    if (this.state.instruction.currentIndex < INSTRUCTIONS.length) {
-      switch (INSTRUCTIONS[this.state.instruction.currentIndex]) {
-        case Instruction.Forward:
-          const delta = new Phaser.Math.Vector2(0, UNIT_LENGTH).setAngle(this.state.player.angle);
-          const target = new Phaser.Math.Vector2(
-            X_OFFSET + this.state.player.col * UNIT_LENGTH,
-            Y_OFFSET + this.state.player.row * UNIT_LENGTH
-          ).add(delta);
-
-          const distance = Phaser.Math.Distance.BetweenPoints(this.player, target);
-          if (distance < TOLERANCE) {
-            (this.player.body as unknown as Phaser.Physics.Arcade.Body).reset(target.x, target.y);
-            this.state.instruction.currentIndex += 1;
-          } else {
-            this.physics.moveTo(this.player, target.x, target.y);
-          }
-          break;
-        case Instruction.RotateCW:
-          if (this.player.angle === Phaser.Math.Angle.WrapDegrees(this.state.player.angle + 90)) {
-            this.state.instruction.currentIndex += 1;
-          } else {
-          }
-          break;
-        case Instruction.RotateCCW:
-          if (this.player.angle === Phaser.Math.Angle.WrapDegrees(this.state.player.angle - 90)) {
-            this.state.instruction.currentIndex += 1;
-          }
-          break;
-        default:
-          throw `Invalid instruction`;
-      }
-    }
-  }
+  update(): void {}
 }
