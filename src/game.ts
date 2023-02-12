637f4b3cf398b44aedc8fbb9a1719b64717a9c62
Justin Chang <justin.p.chang@gmail.com>
2023-02-11 19:40:34 -0500

    Add smooth resize

--- src/game.ts
+++ src/game.ts
@@ -1,22 +1,76 @@
 import Phaser from "phaser";
 
 import PreloaderScene from "./scenes/PreloaderScene";
-import InitialScene from "./scenes/InitialScene";
 import GameScene from "./scenes/GameScene";
 import ProgramScene from "./scenes/ProgramScene";
+
+const DEFAULT_WIDTH = 800;
+const DEFAULT_HEIGHT = 600;
+const MAX_WIDTH = DEFAULT_WIDTH * 1.5;
+const MAX_HEIGHT = DEFAULT_HEIGHT * 1.5;
+const MAX_SMOOTH_SCALE = 1.15;
 
 window.addEventListener("load", () => {
   const config: Phaser.Types.Core.GameConfig = {
     parent: "game",
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    backgroundColor: "#000111",
+    scale: {
+      mode: Phaser.Scale.NONE,
+      width: DEFAULT_WIDTH,
+      height: DEFAULT_HEIGHT,
+    },
     physics: {
       default: "arcade",
+      arcade: {
+        debug: false,
+      },
     },
-    scene: [PreloaderScene, InitialScene, GameScene, ProgramScene],
-    backgroundColor: "#000111",
+    scene: [PreloaderScene, GameScene, ProgramScene],
   };
 
   const game = new Phaser.Game(config);
+
+  const smoothResize = () => {
+    const w = window.innerWidth;
+    const h = window.innerHeight;
+
+    const scale = Math.min(w / DEFAULT_WIDTH, h / DEFAULT_HEIGHT);
+    const newWidth = Math.min(w / scale, MAX_WIDTH);
+    const newHeight = Math.min(h / scale, MAX_HEIGHT);
+
+    const windowRatio = w / h;
+    const defaultRatio = DEFAULT_WIDTH / DEFAULT_HEIGHT;
+    const maxRatioWidth = MAX_WIDTH / DEFAULT_HEIGHT;
+    const maxRatioHeight = MAX_HEIGHT / DEFAULT_WIDTH;
+
+    let smooth: number;
+    const normalize = (value: number, min: number, max: number) => {
+      return (value - min) / (max - min);
+    };
+
+    if (defaultRatio < windowRatio) {
+      smooth =
+        -normalize(newWidth / newHeight, defaultRatio, maxRatioWidth) /
+          (1 / (MAX_SMOOTH_SCALE - 1)) +
+        MAX_SMOOTH_SCALE;
+    } else {
+      smooth =
+        -normalize(newWidth / newHeight, defaultRatio, maxRatioHeight) /
+          (1 / (MAX_SMOOTH_SCALE - 1)) +
+        MAX_SMOOTH_SCALE;
+    }
+
+    game.scale.resize(newWidth * smooth, newHeight * smooth);
+
+    game.canvas.style.width = newWidth * scale + "px";
+    game.canvas.style.height = newHeight * scale + "px";
+
+    game.canvas.style.marginTop = `${(h - newHeight * scale) / 2}px`;
+    game.canvas.style.marginLeft = `${(w - newWidth * scale) / 2}px`;
+  };
+
+  window.addEventListener("resize", smoothResize);
+
+  smoothResize();
 });
