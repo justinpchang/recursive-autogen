8eed6c104345f265b08f4199b1b9dad8f16df491
Justin Chang <justin.p.chang@gmail.com>
2023-02-11 19:54:35 -0500

    Add PWA capability

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 const HtmlPlugin = require("html-webpack-plugin");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 module.exports = {
   entry: "./src/game.ts",
@@ -29,7 +30,14 @@ module.exports = {
   plugins: [
     new HtmlPlugin({ template: "src/index.html" }),
     new CopyPlugin({
-      patterns: [{ from: "src/assets", to: "assets" }],
+      patterns: [
+        { from: "src/assets", to: "assets" },
+        { from: "src/pwa", to: "" },
+      ],
+    }),
+    new InjectManifest({
+      swSrc: path.resolve(__dirname, "../src/pwa/sw.js"),
+      maximumFileSizeToCacheInBytes: 20 * 1024 * 1024,
     }),
   ],
 };
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,35 +1,35 @@
-const path = require('path')
-const merge = require('webpack-merge')
-const common = require('./webpack.common')
-const JavaScriptObfuscator = require('webpack-obfuscator')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+const path = require("path");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common");
+const JavaScriptObfuscator = require("webpack-obfuscator");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 const prod = {
-  mode: 'production',
+  mode: "production",
   output: {
-    filename: 'game.[contenthash].js'
+    filename: "game.[contenthash].js",
   },
   optimization: {
     splitChunks: {
       cacheGroups: {
         commons: {
-          filename: '[name].[contenthash].js'
-        }
-      }
-    }
+          filename: "[name].[contenthash].js",
+        },
+      },
+    },
   },
   plugins: [
-    new CleanWebpackPlugin(['dist/*.js'], { root: path.resolve(__dirname, '../') }),
+    new CleanWebpackPlugin(["dist/*.js"], { root: path.resolve(__dirname, "../") }),
     new JavaScriptObfuscator(
       {
         rotateStringArray: true,
         stringArray: true,
         // stringArrayEncoding: 'base64', // disabled by default
-        stringArrayThreshold: 0.75
+        stringArrayThreshold: 0.75,
       },
-      ['vendors.*.js']
-    )
-  ]
-}
+      ["vendors.*.js"]
+    ),
+  ],
+};
 
-module.exports = merge(common, prod)
+module.exports = merge(common, prod);
